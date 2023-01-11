40525b8cac888e8b031a46a508bf2ca82e6a74aa
Dan Isamu <danilo.isamu@estudante.ufscar.br>
2023-01-11 14:24:41 -0300

    check game over

--- interface.py
+++ interface.py
@@ -19,8 +19,10 @@ action_interval = 1
 image_count = 10
 
 time.sleep(3)
+game_over = False
 while True:
     images = []
+    game_over = False
     for i in range(image_count):
         im = pyautogui.screenshot('my_screenshot' + str(i) + '.png',
                                   region=(350,325, 650, 105))
@@ -28,7 +30,15 @@ while True:
         tensor_im = convert_tensor(im)
         time.sleep(action_interval/image_count)
         images.append(tensor_im)
-        
+
+        if i > 0 and torch.all(torch.eq(tensor_im, images[i-1])):
+            game_over = True
+            break
+
+    if game_over:
+        break
+    
     take_action(images)
 
+print("Game over!")
 
