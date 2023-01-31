ffa843baf51aa9e32c3b7ddd67962c0c5120de1f
Dan Isamu <danilo.isamu@estudante.ufscar.br>
2023-01-31 20:25:24 -0300

    mudado para numpy e opencv

--- interface.py
+++ interface.py
@@ -1,18 +1,20 @@
-import pyautogui
-import torch
-from torchvision import transforms
-from PIL import Image
+import mss.tools
 import time
+import cv2
+import numpy
+import pyautogui
+from PIL import Image
 from intelligence import evaluate_action
+
+#wsh = comclt.Dispatch("WScript.Shell")
 
 
 def take_action(images):
     action_value = evaluate_action(images)
-    match(action_value):
-        case 1:
-            pyautogui.press('up')
-        case -1:
-            pyautogui.press('down') 
+    if action_value == 1:    
+        pyautogui.press('a')
+    elif action_value -1:
+        pyautogui.press('b') 
 
 
 action_interval = 1
@@ -23,22 +25,27 @@ game_over = False
 while True:
     images = []
     game_over = False
-    for i in range(image_count):
-        im = pyautogui.screenshot('my_screenshot' + str(i) + '.png',
-                                  region=(350,325, 650, 105))
-        convert_tensor = transforms.ToTensor()
-        tensor_im = convert_tensor(im)
-        time.sleep(action_interval/image_count)
-        images.append(tensor_im)
 
-        if i > 0 and torch.all(torch.eq(tensor_im, images[i-1])):
-            game_over = True
+    for i in range(10):
+        with mss.mss() as sct:
+            # The screen part to capture
+            monitor = {"top": 160, "left": 160, "width": 160, "height": 135}
+            output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
+
+            # Grab the data
+            sct_img = sct.grab(monitor)
+            img = Image.new("RGB", sct_img.size)
+
+            # Save to the picture file
+            #mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+            images.append(img)
+            print(output)
+
+        if game_over:
             break
-
-    if game_over:
-        break
-    
+        
     take_action(images)
+    break
 
 print("Game over!")
 
